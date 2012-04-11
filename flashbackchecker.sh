51ab82f0eba7d5e2adb340c3902a6f2f38dbb215
David Dvorak <dzd1@humboldt.edu>
2012-04-10 23:14:04 -0700

    flashbackchecker sh added

---
+++ flashbackchecker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+defaults read /Applications/Safari.app/Contents/Info LSEnvironment
+echo "If no error message YOU GOT INFECTED!"
+
+defaults read ~/.MacOSX/environment DYLD_INSERT_LIBRARIES
+echo "If no error message YOU GOT INFECTED!"
